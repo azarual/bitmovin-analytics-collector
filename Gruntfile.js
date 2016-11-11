99bbf5ecac5237008064072832bcab40d17cce41
Daniel Hölbling-Inzko <daniel.hoelbling-inzko@bitmovin.com>
2016-11-11 13:08:46 +0100

    Added grunt tasks

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*!- <%= pkg.name %> <%= pkg.version %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        files: {
+          'build/<%= pkg.name %>-jw.min.js': ['build/*-jw.js'],
+          'build/<%= pkg.name %>-bitmovin.min.js': ['build/*-bitmovin.js'],
+          'build/<%= pkg.name %>-radiant.min.js': ['build/*-radiant.js']
+        }
+      }
+    },
+    concat: {
+      options: {
+        seperator: ';'
+      },
+      build: {
+        files: {
+          'build/<%= pkg.name %>-bitmovin.js': ['js/bitanalytics-core.js', 'js/bitanalytics-bitmovin.js'],
+          'build/<%= pkg.name %>-jw.js': ['js/bitanalytics-core.js', 'js/bitanalytics-jw.js'],
+          'build/<%= pkg.name %>-radiant.js': ['js/bitanalytics-core.js', 'js/bitanalytics-radiant.js']
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  //grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.registerTask('default', ['concat', 'uglify']);
+};
