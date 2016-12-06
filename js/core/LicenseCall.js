c3063c3813a740976f9d66c58ad163034167e1ed
Daniel Hölbling-Inzko <daniel.hoelbling-inzko@bitmovin.com>
2016-12-06 17:14:49 +0100

    Fixed IE9 issue in licensing call

--- js/core/LicenseCall.js
+++ js/core/LicenseCall.js
@@ -3,7 +3,7 @@
  */
 
 function LicenseCall() {
-  var licenseServerUrl = 'https://bitmovin-bitanalytics.appspot.com/licensing';
+  var licenseServerUrl = '//bitmovin-bitanalytics.appspot.com/licensing';
 
   this.sendRequest = function(key, domain, version, callback) {
     var licensingRequest = {
@@ -29,4 +29,27 @@ function LicenseCall() {
     xhttp.setRequestHeader('Content-Type', 'application/json');
     xhttp.send(JSON.stringify(licensingRequest));
   };
+  if (window.XDomainRequest) {
+    this.sendRequest = function (key, domain, version, callback) {
+
+      var licensingRequest = {
+        key: key,
+        domain: domain,
+        analyticsVersion: version
+      };
+
+      var xdr = new XDomainRequest();
+      xdr.onload = function () {
+        if (xdr.responseText.length <= 0) {
+          return;
+        }
+
+        var licensingResponse = JSON.parse(xdr.responseText);
+        callback(licensingResponse);
+      };
+
+      xdr.open('POST', licenseServerUrl, true);
+      xdr.send(JSON.stringify(licensingRequest));
+    };
+  }
 }
