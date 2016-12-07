329e687eb6a9130d2d8df9d1741fc9ed4680b1c2
lukaskroepfl <lukas.kroepfl@bitmovin.com>
2016-12-07 11:26:04 +0100

    Changed jw and radiant adapters to work with the new adapter structure.

--- js/adapters/bitanalytics-jw.js
+++ js/adapters/bitanalytics-jw.js
@@ -2,155 +2,146 @@
  * Created by Bitmovin on 19.09.2016.
  */
 
-/**
- * Note
- * Ads are also not working due to a missing JW advertising license
- */
+function Adapter(record) {
+  var skipInitResize = true;
+  var levelsChanged  = false;
+  var firstSample    = true;
 
+  this.register = function(player) {
+    player.on('playlist', function() {
+      record(analytics.events.SOURCE_LOADED);
+    });
 
-/**
- *
- * @type {boolean}
- * variable to skip init resize event
- */
-var skipInitResize = true;
-var levelsChanged  = false;
-var firstSample    = true;
+    player.on('ready', function() {
+      var streamType;
+      if (player.getProvider().name == 'shaka') {
+        streamType = 'DASH';
+      }
+      else {
+        streamType = 'unknown';
+      }
 
-function registerEvents(player) {
-  player.on('playlist', function() {
-    analytics.record(analytics.events.SOURCE_LOADED);
-  });
+      record(analytics.events.READY, {
+        /**
+         *  jw player is always rendering in html5 mode
+         *  according to https://github.com/jwplayer/jwplayer/wiki/2.2-JW-Player-API-Reference#getters
+         */
+        type      : 'html5',
+        version   : player.version.substring(0, 5),
+        duration  : player.getDuration(),
+        streamType: streamType
+        //isLive:     player.isLive(),
+        //videoId:    player.getConfig().source.videoId,
+        //userId:     player.getConfig().source.userId
+      });
+    });
 
-  player.on('ready', function() {
-    var streamType;
-    if (player.getProvider().name == 'shaka') {
-      streamType = 'DASH';
-    }
-    else {
-      streamType = 'unknown';
-    }
+    player.on('play', function() {
+      record(analytics.events.READY, {
+        duration: player.getDuration()
+      });
+      record(analytics.events.PLAY);
+    });
 
-    analytics.record(analytics.events.READY, {
+    player.on('pause', function() {
+      if (!levelsChanged && !firstSample) {
+        record(analytics.events.PAUSE, {
+          currentTime: player.getPosition()
+        });
+      }
+      else {
+        levelsChanged = false;
+      }
+    });
+
+    player.on('levelsChanged', function() {
+      levelsChanged = true;
+    });
+
+    player.on('time', function() {
+      firstSample    = false;
+      skipInitResize = false;
+
+      record(analytics.events.TIMECHANGED, {
+        currentTime: player.getPosition()
+      });
+    });
+
+    player.on('audioTrackChanged', function(event) {
+      console.log('AUDIO: ' + JSON.stringify(event));
+    });
+
+    player.on('visualQuality', function(event) {
+      record(analytics.events.VIDEO_CHANGE, {
+        width      : event.level.width,
+        height     : event.level.height,
+        bitrate    : event.level.bitrate,
+        currentTime: player.getPosition()
+      });
+    });
+
+    player.on('seek', function() {
+      record(analytics.events.SEEK, {
+        currentTime: player.getPosition()
+      });
+    });
+
+    player.on('bufferChange', function() {
+      record(analytics.events.START_BUFFERING, {
+        currentTime: player.getPosition()
+      });
+    });
+
+    player.on('seeked', function() {
+      record(analytics.events.END_BUFFERING, {
+        currentTime: player.getPosition()
+      });
+    });
+
+    player.on('qualityChange', function(event) {
+      console.log(JSON.stringify(event));
+    });
+
+    player.on('fullscreen', function(event) {
+      if (event.fullscreen == true) {
+        record(analytics.events.START_FULLSCREEN, {
+          currentTime: player.getPosition()
+        });
+      }
+      else {
+        record(analytics.events.END_FULLSCREEN, {
+          currentTime: player.getPosition()
+        });
+      }
+    });
+
+    player.on('resize', function() {
       /**
-       *  jw player is always rendering in html5 mode
-       *  according to https://github.com/jwplayer/jwplayer/wiki/2.2-JW-Player-API-Reference#getters
+       *  this event is thrown before on the first time change event
+       *  so we have to skip it the first time
        */
-      type      : 'html5',
-      version   : player.version.substring(0, 5),
-      duration  : player.getDuration(),
-      streamType: streamType
-      //isLive:     player.isLive(),
-      //videoId:    player.getConfig().source.videoId,
-      //userId:     player.getConfig().source.userId
+      if (!skipInitResize) {
+        record(analytics.events.SCREEN_RESIZE, {
+          currentTime: player.getPosition()
+        });
+      }
     });
-  });
 
-  player.on('play', function() {
-    analytics.record(analytics.events.READY, {
-      duration: player.getDuration()
-    });
-    analytics.record(analytics.events.PLAY);
-  });
-
-  player.on('pause', function() {
-    if (!levelsChanged && !firstSample) {
-      analytics.record(analytics.events.PAUSE, {
+    player.on('error', function(event) {
+      record(analytics.events.ERROR, {
+        message    : event.message,
         currentTime: player.getPosition()
       });
-    }
-    else {
-      levelsChanged = false;
-    }
-  });
-
-  player.on('levelsChanged', function() {
-    levelsChanged = true;
-  });
-
-  player.on('time', function() {
-    firstSample    = false;
-    skipInitResize = false;
-
-    analytics.record(analytics.events.TIMECHANGED, {
-      currentTime: player.getPosition()
     });
-  });
 
-  player.on('audioTrackChanged', function(event) {
-    console.log('AUDIO: ' + JSON.stringify(event));
-  });
-
-  player.on('visualQuality', function(event) {
-    analytics.record(analytics.events.VIDEO_CHANGE, {
-      width      : event.level.width,
-      height     : event.level.height,
-      bitrate    : event.level.bitrate,
-      currentTime: player.getPosition()
+    player.on('complete', function() {
+      record(analytics.events.PLAYBACK_FINISHED);
     });
-  });
 
-  player.on('seek', function() {
-    analytics.record(analytics.events.SEEK, {
-      currentTime: player.getPosition()
-    });
-  });
-
-  player.on('bufferChange', function() {
-    analytics.record(analytics.events.START_BUFFERING, {
-      currentTime: player.getPosition()
-    });
-  });
-
-  player.on('seeked', function() {
-    analytics.record(analytics.events.END_BUFFERING, {
-      currentTime: player.getPosition()
-    });
-  });
-
-  player.on('qualityChange', function(event) {
-    console.log(JSON.stringify(event));
-  });
-
-  player.on('fullscreen', function(event) {
-    if (event.fullscreen == true) {
-      analytics.record(analytics.events.START_FULLSCREEN, {
+    window.addEventListener('unload', function() {
+      record(analytics.events.UNLOAD, {
         currentTime: player.getPosition()
       });
-    }
-    else {
-      analytics.record(analytics.events.END_FULLSCREEN, {
-        currentTime: player.getPosition()
-      });
-    }
-  });
-
-  player.on('resize', function() {
-    /**
-     *  this event is thrown before on the first time change event
-     *  so we have to skip it the first time
-     */
-    if (!skipInitResize) {
-      analytics.record(analytics.events.SCREEN_RESIZE, {
-        currentTime: player.getPosition()
-      });
-    }
-  });
-
-  player.on('error', function(event) {
-    analytics.record(analytics.events.ERROR, {
-      message    : event.message,
-      currentTime: player.getPosition()
     });
-  });
-
-  player.on('complete', function() {
-    analytics.record(analytics.events.PLAYBACK_FINISHED);
-  });
-
-  window.addEventListener('unload', function() {
-    analytics.record(analytics.events.UNLOAD, {
-      currentTime: player.getPosition()
-    });
-  });
+  };
 }
--- js/adapters/bitanalytics-radiant.js
+++ js/adapters/bitanalytics-radiant.js
@@ -2,144 +2,140 @@
  * Created by Bitmovin on 23.09.2016.
  */
 
-/**
- * prevent player raising init seeking event
- * just once
- */
-var skipInitSeek = true;
+function registerEvents(record) {
+  var skipInitSeek = true;
+  var lastQuality = null;
 
-var lastQuality = null;
-
-function registerEvents(container, player) {
-  container.addEventListener('loadstart', function() {
-    analytics.record(analytics.events.SOURCE_LOADED);
-  });
-
-  container.addEventListener('ready', function() {
-    analytics.record(analytics.events.READY, {
-      type      : player.getPlayerMode(),
-      version   : player.getPlayerVersion(),
-      streamType: player.getStreamType()
-      //isLive:     player.isLive(),
-      //videoId:    player.getConfig().source.videoId,
-      //userId:     player.getConfig().source.userId
+  this.register = function(player) {
+    player.addEventListener('loadstart', function() {
+      record(analytics.events.SOURCE_LOADED);
     });
-  });
 
-  container.addEventListener('durationchange', function() {
-    analytics.record(analytics.events.READY, {
-      duration: toSeconds(player.getDuration())
+    player.addEventListener('ready', function() {
+      record(analytics.events.READY, {
+        type      : player.getPlayerMode(),
+        version   : player.getPlayerVersion(),
+        streamType: player.getStreamType()
+        //isLive:     player.isLive(),
+        //videoId:    player.getConfig().source.videoId,
+        //userId:     player.getConfig().source.userId
+      });
     });
-  });
 
-  container.addEventListener('play', function() {
-    analytics.record(analytics.events.PLAY);
-  });
+    player.addEventListener('durationchange', function() {
+      record(analytics.events.READY, {
+        duration: toSeconds(player.getDuration())
+      });
+    });
 
-  container.addEventListener('timeupdate', function() {
+    player.addEventListener('play', function() {
+      record(analytics.events.PLAY);
+    });
 
-    /**
-     * get video qualities
-     */
-    var quality = player.getBitrates()[player.getCurrentBitrateIndex()];
-    if (JSON.stringify(quality) != JSON.stringify(lastQuality)) {
-      analytics.record(analytics.events.VIDEO_CHANGE, {
-        width      : quality.width,
-        height     : quality.height,
-        bitrate    : quality.bitrate,
+    player.addEventListener('timeupdate', function() {
+
+      /**
+       * get video qualities
+       */
+      var quality = player.getBitrates()[player.getCurrentBitrateIndex()];
+      if (JSON.stringify(quality) != JSON.stringify(lastQuality)) {
+        record(analytics.events.VIDEO_CHANGE, {
+          width      : quality.width,
+          height     : quality.height,
+          bitrate    : quality.bitrate,
+          currentTime: toSeconds(player.getCurrentTime())
+        });
+        lastQuality = quality;
+      }
+
+      record(analytics.events.TIMECHANGED, {
         currentTime: toSeconds(player.getCurrentTime())
       });
-      lastQuality = quality;
-    }
-
-    analytics.record(analytics.events.TIMECHANGED, {
-      currentTime: toSeconds(player.getCurrentTime())
     });
-  });
 
-  container.addEventListener('pause', function() {
+    player.addEventListener('pause', function() {
 
-    /**
-     * prevent player to raise pause event during playback finish
-     */
-    if (player.getCurrentTime() < player.getDuration()) {
-      analytics.record(analytics.events.PAUSE, {
+      /**
+       * prevent player to raise pause event during playback finish
+       */
+      if (player.getCurrentTime() < player.getDuration()) {
+        record(analytics.events.PAUSE, {
+          currentTime: toSeconds(player.getCurrentTime())
+        });
+      }
+    });
+
+    player.addEventListener('seeking', function() {
+      if (!skipInitSeek) {
+        record(analytics.events.SEEK, {
+          currentTime: toSeconds(player.getCurrentTime())
+        });
+      }
+    });
+
+    player.addEventListener('waiting', function() {
+      record(analytics.events.START_BUFFERING, {
         currentTime: toSeconds(player.getCurrentTime())
       });
-    }
-  });
+    });
 
-  container.addEventListener('seeking', function() {
-    if (!skipInitSeek) {
-      analytics.record(analytics.events.SEEK, {
+    player.addEventListener('seeked', function() {
+      if (!skipInitSeek) {
+        record(analytics.events.END_BUFFERING, {
+          currentTime: toSeconds(player.getCurrentTime())
+        });
+      }
+      else {
+        skipInitSeek = false;
+      }
+
+    });
+
+    player.addEventListener('enterfullscreen', function() {
+      record(analytics.events.START_FULLSCREEN, {
         currentTime: toSeconds(player.getCurrentTime())
       });
-    }
-  });
-
-  container.addEventListener('waiting', function() {
-    analytics.record(analytics.events.START_BUFFERING, {
-      currentTime: toSeconds(player.getCurrentTime())
     });
-  });
 
-  container.addEventListener('seeked', function() {
-    if (!skipInitSeek) {
-      analytics.record(analytics.events.END_BUFFERING, {
+    player.addEventListener('exitfullscreen', function() {
+      record(analytics.events.END_FULLSCREEN, {
         currentTime: toSeconds(player.getCurrentTime())
       });
-    }
-    else {
-      skipInitSeek = false;
-    }
-
-  });
-
-  container.addEventListener('enterfullscreen', function() {
-    analytics.record(analytics.events.START_FULLSCREEN, {
-      currentTime: toSeconds(player.getCurrentTime())
     });
-  });
 
-  container.addEventListener('exitfullscreen', function() {
-    analytics.record(analytics.events.END_FULLSCREEN, {
-      currentTime: toSeconds(player.getCurrentTime())
+    player.addEventListener('error', function() {
+      record(analytics.events.ERROR, {
+        message    : 'Radiant error occurs',
+        currentTime: player.getCurrentTime()
+      });
     });
-  });
 
-  container.addEventListener('error', function() {
-    analytics.record(analytics.events.ERROR, {
-      message    : 'Radiant error occurs',
-      currentTime: player.getCurrentTime()
+    rmpplayer.addEventListener('ended', function() {
+      record(analytics.events.PLAYBACK_FINISHED);
     });
-  });
 
-  rmpContainer.addEventListener('ended', function() {
-    analytics.record(analytics.events.PLAYBACK_FINISHED);
-  });
-
-  container.addEventListener('qualitychangestarted', function() {
-    console.log('SWITCH');
-  });
-
-  container.addEventListener('adloaded', function() {
-    analytics.record(analytics.events.START_AD);
-  });
-
-  container.addEventListener('adcomplete', function() {
-    analytics.record(analytics.events.END_AD, {
-      currentTime: toSeconds(player.getCurrentTime())
+    player.addEventListener('qualitychangestarted', function() {
+      console.log('SWITCH');
     });
-  });
 
-  window.addEventListener('unload', function() {
-    analytics.record(analytics.events.UNLOAD, {
-      currentTime: toSeconds(player.getCurrentTime())
+    player.addEventListener('adloaded', function() {
+      record(analytics.events.START_AD);
     });
-  });
-}
 
-function toSeconds(time) {
+    player.addEventListener('adcomplete', function() {
+      record(analytics.events.END_AD, {
+        currentTime: toSeconds(player.getCurrentTime())
+      });
+    });
 
-  return time / 1000;
+    window.addEventListener('unload', function() {
+      record(analytics.events.UNLOAD, {
+        currentTime: toSeconds(player.getCurrentTime())
+      });
+    });
+  };
+
+  function toSeconds(time) {
+    return time / 1000;
+  }
 }
