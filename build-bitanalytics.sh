9d7a11f65f6db69c8454b9f774f24b2a46985cc2
lukaskroepfl <lukas.kroepfl@bitmovin.com>
2016-12-12 14:46:10 +0100

    Removed unused build script.

--- build-bitanalytics.sh
+++
@@ -1,4 +0,0 @@
-mkdir temp
-cat js/bitanalytics-core.js js/bitanalytics-bitmovin.js > temp/bundle.js
-uglifyjs --compress --source-map build/bitanalytics.min.js.map temp/bundle.js > build/bitanalytics.min.js
-rm -r temp
