804ccbdd2a595d4804fefb58332c8eed57e5a9f5
lukaskroepfl <lukas.kroepfl@bitmovin.com>
2017-01-12 11:05:35 +0100

    Implemented player version detection for bitmovin player.

--- js/core/AdapterFactory.js
+++ js/core/AdapterFactory.js
@@ -3,8 +3,28 @@
  */
 
 var AdapterFactory = function() {
+  var isBitmovinVersionPre7 = function(player) {
+    if (typeof player.getVersion === 'function') {
+      return player.getVersion() < '7';
+    }
+
+    return false;
+  };
+
+  var isBitmovinVersion7Plus = function(player) {
+    if (typeof player.version === 'string') {
+      return player.version >= '7';
+    }
+
+    return false;
+  };
+
   var getAdapter = function(player) {
-    return new Bitmovin7Adapter(player);
+    if (isBitmovinVersionPre7(player)) {
+      return new BitmovinAdapter(player);
+    } else if (isBitmovinVersion7Plus(player)) {
+      return new Bitmovin7Adapter(player);
+    }
   };
 
   return {
