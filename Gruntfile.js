f05598d67ace544260b84c396eb82d0169f6749a
lukaskroepfl <lukas.kroepfl@bitmovin.com>
2017-01-12 10:52:46 +0100

    Adapted grunt file for new adapter structure.

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,9 +24,7 @@ module.exports = function(grunt) {
       },
       build: {
         files: {
-          'build/bitmovin/<%= pkg.name %>.min.js': ['build/bitmovin/*.js'],
-          'build/jw/<%= pkg.name %>.min.js': ['build/jw/*.js'],
-          'build/radiant/<%= pkg.name %>.min.js': ['build/radiant/*.js']
+          'build/<%= pkg.name %>.min.js': ['build/*.js']
         }
       }
     },
@@ -38,40 +36,16 @@ module.exports = function(grunt) {
       },
       build: {
         files: {
-          'build/bitmovin/<%= pkg.name %>.js': [
+          'build/<%= pkg.name %>.js': [
             'node_modules/javascript-state-machine/state-machine.js',
             'js/core/AnalyticsCall.js',
             'js/core/AnalyticsStateMachine.js',
             'js/core/LicenseCall.js',
             'js/core/Logger.js',
             'js/core/Utils.js',
+            'js/core/AdapterFactory.js',
             'js/adapters/bitanalytics-bitmovin.js',
-            'js/core/Bitanalytics.js',
-            'js/core/Events.js',
-            'js/core/Players.js',
-            'js/core/CDNProviders.js'
-          ],
-          'build/jw/<%= pkg.name %>.js': [
-            'node_modules/javascript-state-machine/state-machine.js',
-            'js/core/AnalyticsCall.js',
-            'js/core/AnalyticsStateMachine.js',
-            'js/core/LicenseCall.js',
-            'js/core/Logger.js',
-            'js/core/Utils.js',
-            'js/adapters/bitanalytics-jw.js',
-            'js/core/Bitanalytics.js',
-            'js/core/Events.js',
-            'js/core/Players.js',
-            'js/core/CDNProviders.js'
-          ],
-          'build/radiant/<%= pkg.name %>.js': [
-            'node_modules/javascript-state-machine/state-machine.js',
-            'js/core/AnalyticsCall.js',
-            'js/core/AnalyticsStateMachine.js',
-            'js/core/LicenseCall.js',
-            'js/core/Logger.js',
-            'js/core/Utils.js',
-            'js/adapters/bitanalytics-radiant.js',
+            'js/adapters/bitanalytics-bitmovin-7.js',
             'js/core/Bitanalytics.js',
             'js/core/Events.js',
             'js/core/Players.js',
@@ -96,20 +70,8 @@ module.exports = function(grunt) {
           {
             expand: true,
             flatten: true,
-            src: ['build/bitmovin/*.js'],
-            dest: 'build/bitmovin'
-          },
-          {
-            expand: true,
-            flatten: true,
-            src: ['build/jw/*.js'],
-            dest: 'build/jw'
-          },
-          {
-            expand: true,
-            flatten: true,
-            src: ['build/radiant/*.js'],
-            dest: 'build/radiant'
+            src: ['build/*.js'],
+            dest: 'build'
           }
         ]
       }
