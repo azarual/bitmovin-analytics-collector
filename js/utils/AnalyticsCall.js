dabe867585047d540b6437df64061ce7cd87c720
lukaskroepfl <lukas.kroepfl@bitmovin.com>
2017-01-13 08:54:21 +0100

    Crated base class HttpCall.

--- js/utils/AnalyticsCall.js
+++ js/utils/AnalyticsCall.js
@@ -1,60 +1,21 @@
 /**
  * Created by lkroepfl on 11.11.16.
  */
+import HttpCall from './HttpCall'
 
-class AnalyticsCall {
+class AnalyticsCall extends HttpCall{
   static analyticsServerUrl = '//bitmovin-bitanalytics.appspot.com/analytics';
 
   sendRequest = function(sample, callback) {
-    this.sendSampleRequest(true, sample, callback);
+    this.post(this.analyticsServerUrl, sample, callback);
   };
 
   sendRequestSynchronous = function(sample, callback) {
-    this.sendSampleRequest(false, sample, callback);
+    this.post(this.analyticsServerUrl, sample, callback, false);
   };
 
   getAnalyticsServerUrl = function() {
     return this.analyticsServerUrl;
-  };
-
-  sendSampleRequest(async, sample, callback) {
-    let xhttp;
-    let legacyMode = false;
-
-    if (window.XDomainRequest) {
-      legacyMode = true;
-    }
-
-    if (legacyMode) {
-      xhttp = new window.XDomainRequest();
-    } else {
-      xhttp = new XMLHttpRequest();
-    }
-
-    const responseCallback = function() {
-      if (xhttp.readyState == XMLHttpRequest.DONE) {
-        if (xhttp.responseText <= 0) {
-          return;
-        }
-
-        const sampleResponse = JSON.parse(xhttp.responseText);
-
-        callback(sampleResponse);
-      }
-    };
-
-    if (legacyMode) {
-      xhttp.onload = responseCallback;
-    } else {
-      xhttp.onreadystatechange = responseCallback;
-    }
-
-
-    xhttp.open('POST', this.analyticsServerUrl, async);
-    if (!legacyMode) {
-      xhttp.setRequestHeader('Content-Type', 'application/json');
-    }
-    xhttp.send(JSON.stringify(sample));
   }
 }
 
--- js/utils/LicenseCall.js
+++ js/utils/LicenseCall.js
@@ -1,8 +1,9 @@
 /**
  * Created by lkroepfl on 11.11.16.
  */
+import HttpCall from './HttpCall'
 
-class LicenseCall {
+class LicenseCall extends HttpCall {
   static licenseServerUrl = '//bitmovin-bitanalytics.appspot.com/licensing';
 
   sendRequest = function(key, domain, version, callback) {
@@ -12,46 +13,8 @@ class LicenseCall {
       analyticsVersion: version
     };
 
-    this.sendLicensingRequest(true, licensingRequest, callback);
+    this.post(this.licenseServerUrl, licensingRequest, callback);
   };
-
-  sendLicensingRequest(async, licensingSample, callback) {
-    let xhttp;
-    let legacyMode = false;
-
-    if (window.XDomainRequest) { legacyMode = true; }
-
-    if (legacyMode) {
-      xhttp = new window.XDomainRequest();
-    } else {
-      xhttp = new XMLHttpRequest();
-    }
-
-    const responseCallback = function() {
-      if (xhttp.readyState == XMLHttpRequest.DONE) {
-        if (xhttp.responseText <= 0) {
-          return;
-        }
-
-        const sampleResponse = JSON.parse(xhttp.responseText);
-
-        callback(sampleResponse);
-      }
-    };
-
-    if (legacyMode) {
-      xhttp.onload = responseCallback;
-    } else {
-      xhttp.onreadystatechange = responseCallback;
-    }
-
-
-    xhttp.open('POST', this.licenseServerUrl, async);
-    if (!legacyMode) {
-      xhttp.setRequestHeader('Content-Type', 'application/json');
-    }
-    xhttp.send(JSON.stringify(licensingSample));
-  }
 }
 
 export default LicenseCall
