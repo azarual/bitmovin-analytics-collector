4bdae7052225977f23a3a38e675926f3b8a13038
lukaskroepfl <lukas.kroepfl@bitmovin.com>
2017-01-13 09:46:28 +0100

    Added imports and fixed instanciation

--- js/core/AdapterFactory.js
+++ js/core/AdapterFactory.js
@@ -4,15 +4,16 @@
 
 import PlayerDetector from '../utils/PlayerDetector'
 import BitmovinAdapter from '../adapters/BitmovinAdapter'
+import Bitmovin7Adapter from '../adapters/Bitmovin7Adapter'
 
 class AdapterFactory {
   static playerDetector = new PlayerDetector;
 
-  getAdapter = function(player) {
+  getAdapter(player, eventCallback) {
     if (this.playerDetector.isBitmovinVersionPre7(player)) {
-      return new BitmovinAdapter(player);
+      return new BitmovinAdapter(player, eventCallback);
     } else if (this.playerDetector.isBitmovinVersion7Plus(player)) {
-      return new Bitmovin7Adapter(player);
+      return new Bitmovin7Adapter(player, eventCallback);
     }
   };
 }
