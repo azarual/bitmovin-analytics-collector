64718d63bc5174dd35c7504a154a994309d2b5ff
lukaskroepfl <lukas.kroepfl@bitmovin.com>
2017-02-09 14:46:48 +0100

    added cast client and receiver.

--- js/cast/CastClient.js
+++ js/cast/CastClient.js
@@ -2,37 +2,36 @@
  * Created by lkroepfl on 09.02.17.
  */
 
+import {MESSAGE_NAMESPACE} from '../utils/Settings';
 import Logger from '../utils/Logger';
 
 class RemoteControl {
-  static MESSAGE_NAMESPACE = 'urn:x-cast:com.bitmovin.analytics.cast';
-
   constructor() {
     this.castSession = cast.framework.CastContext.getInstance().getCurrentSession();
     this.addMessageListener();
   }
 
   addMessageListener() {
-    this.castSession.addMessageListener(RemoteControl.MESSAGE_NAMESPACE, (ns, message) => {
+    this.castSession.addMessageListener(MESSAGE_NAMESPACE, (ns, message) => {
       Logger.log('Received: ' + ns + ' ' + message);
 
       try {
         const receiverMessage = JSON.parse(message);
-        this.handleReceivedMessage(receiverMessage);
+        this.handleReceiverMessage(receiverMessage);
 
       } catch (error) {
-        Logger.error('Message parsing failed' + message);
+        Logger.error('Message parsing failed ' + message);
       }
     });
   }
 
-  handleReceivedMessage(message) {
-
+  handleReceiverMessage(message) {
+    Logger.log(message);
   }
 
   sendMessage(message) {
     Logger.log('Sending message: ' + JSON.stringify(message));
-    this.castSession.sendMessage(RemoteControl.MESSAGE_NAMESPACE, message);
+    this.castSession.sendMessage(MESSAGE_NAMESPACE, message);
   }
 }
 
--- js/cast/CastReceiver.js
+++ js/cast/CastReceiver.js
@@ -1,5 +1,36 @@
 /**
  * Created by lkroepfl on 09.02.17.
  */
+import {MESSAGE_NAMESPACE} from '../utils/Settings';
+import Logger from '../utils/Logger';
 
-class RemoteCo
+class CastReceiver {
+
+  constructor() {
+    const castReceiverManager = cast.receiver.CastReceiverManager.getInstance();
+    this.messageBus = castReceiverManager.getCastMessageBus(
+      MESSAGE_NAMESPACE,
+      cast.receiver.CastMessageBus.MessageType.JSON
+    );
+
+    this.messageBus.onMessage = (event) => {
+      Logger.log('customMessageBus', event);
+
+      try {
+        this.handleClientMessage(event);
+      } catch (error) {
+        Logger.error('illegal message', event, error);
+      }
+    };
+  }
+
+  handleClientMessage(event) {
+    Logger.log(event);
+  }
+
+  sendMessage(message) {
+    this.messageBus.broadcast(message);
+  }
+}
+
+export default CastReceiver;
