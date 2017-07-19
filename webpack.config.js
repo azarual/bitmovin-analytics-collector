0ff8e9dd5a234f197b85ad8eec8c883dd1811863
Daniel Hoelbling-Inzko <daniel@hoelbling-inzko.com>
2017-07-19 13:13:24 +0200

    Fixed header in js collector file

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,9 @@ const packageProperties = require('./package.json');
 const getGitVersion = () => {
   return execSync('git describe --abbrev=0').toString().trim()
 }
+const getFullGitVersion = () => {
+  return execSync('git describe').toString().trim()
+}
 const banner =
         '\n' +
         'Copyright (C) ' + new Date().getFullYear() + ', Bitmovin, Inc., All Rights Reserved\n' +
@@ -11,7 +14,7 @@ const banner =
         'This source code and its use and distribution, is subject to the terms\n' +
         'and conditions of the applicable license agreement.\n' +
         '\n' +
-        packageProperties.name + ' version ' + getGitVersion() + '\n';
+        packageProperties.name + ' version ' + getFullGitVersion() + '\n';
 
 const entry = './js/core/BitmovinAnalyticsExport.js';
 
